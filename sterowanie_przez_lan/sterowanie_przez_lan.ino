a5203f69d49b82733a8e5eeac4b113a5a3681b52
Krs <krybski@gmail.com>
2021-07-10 14:51:22 +0200

    Usowanie komunikacji po usb

--- sterowanie_przez_lan/sterowanie_przez_lan.ino
+++ sterowanie_przez_lan/sterowanie_przez_lan.ino
@@ -1,21 +1,21 @@
 /*
   Web Server
 
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
+  A simple web server that shows the value of the analog input pins.
+  using an Arduino Wiznet Ethernet shield.
 
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
+  Circuit:
+   Ethernet shield attached to pins 10, 11, 12, 13
+   Analog inputs attached to pins A0 through A5 (optional)
 
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
- 
- */
+  created 18 Dec 2009
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
+  modified 02 Sept 2015
+  by Arturo Guadalupi
+
+*/
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -26,7 +26,7 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0x90, 0xA2, 0xDA, 0x7A, 0x70, 0x25
 };
 IPAddress ip(192, 168, 100, 232);
 
@@ -41,39 +41,13 @@ void setup() {
   pinMode(swiatlo_2, OUTPUT);
   digitalWrite(swiatlo_2, LOW);
   digitalWrite(swiatlo_1, LOW);
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
-
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("Ethernet WebServer Example");
-
+  
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
 
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
   // start the server
   server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+
 }
 
 
@@ -81,7 +55,7 @@ void loop() {
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
-    Serial.println("new client");
+
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
@@ -98,9 +72,9 @@ void loop() {
         }
         if (request.indexOf("/LED2=OFF") != -1)  {
           digitalWrite(swiatlo_2, LOW);
-        }       
+        }
         char c = client.read();
-        Serial.write(c);
+ 
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
@@ -109,23 +83,23 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-//          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          //          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
           client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
           // output the value of each analog input pin
-          if(digitalRead(swiatlo_1) == LOW){ 
+          if (digitalRead(swiatlo_1) == LOW) {
             client.println("<h1><a href=\"/LED=ON\">LED GARAZ ON</a></h1><br>");
           }
-          if(digitalRead(swiatlo_1) == HIGH){ 
+          if (digitalRead(swiatlo_1) == HIGH) {
             client.println("<h1><a href=\"/LED=OFF\">LED GARAZ OFF</a></h1><br>");
-          }         
+          }
           client.println("<HR>");
-          if(digitalRead(swiatlo_2) == LOW){ 
+          if (digitalRead(swiatlo_2) == LOW) {
             client.println("<h1><a href=\"/LED2=ON\">LED ORZECH ON</a></h1><br>");
           }
-          if(digitalRead(swiatlo_2) == HIGH){ 
+          if (digitalRead(swiatlo_2) == HIGH) {
             client.println("<h1><a href=\"/LED2=OFF\">LED ORZECH OFF</a></h1><br>");
           }
           client.println("</html>");
@@ -141,9 +115,8 @@ void loop() {
       }
     }
     // give the web browser time to receive the data
-    delay(1);
+    delay(10);
     // close the connection:
     client.stop();
-    Serial.println("client disconnected");
   }
 }
